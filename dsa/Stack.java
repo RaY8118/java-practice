b072325dcf9788f0fb8fe702fe5bd9d1de27c41d
RaY8118 <parthghadi43@gmail.com>
2025-06-16 18:28:49 +0000

    Implement Stack data structure with push, pop, peek, and display methods

---
+++ dsa/Stack.java
@@ -0,0 +1,65 @@
+public class Stack {
+  private int[] stack;
+  private int top;
+  private int capacity;
+
+  public Stack(int size) {
+    stack = new int[size];
+    capacity = size;
+    top = -1;
+  }
+
+  public void push(int value) {
+    if (top == capacity - 1) {
+      System.out.println("Stack is overflow");
+    }
+    stack[++top] = value;
+  }
+
+  public int pop() {
+    if (top == -1) {
+      System.out.println("Stack is empty");
+      return -1;
+    }
+    return stack[top--];
+  }
+
+  public int peek() {
+    if (top == -1) {
+      System.out.println("Stack is empty");
+      return -1;
+    }
+    return stack[top];
+  }
+
+  public boolean isEmpty() {
+    return top == -1;
+  }
+
+  public int size() {
+    return top + 1;
+  }
+
+  public void display() {
+    if (top == -1) {
+      System.out.println("Stack is empty");
+    }
+    for (int i = top; i >= 0; i--) {
+      System.out.println(stack[i]);
+    }
+  }
+
+  public static void main(String[] args) {
+    Stack stack = new Stack(5);
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    stack.push(40);
+
+    System.out.println("Top element is:" + stack.peek());
+
+    stack.display();
+
+  }
+}
