4bcc6f5cc4c2920bf2a3d5e32b6a8a95cae04865
RaY8118 <parthghadi43@gmail.com>
2025-11-29 16:23:43 +0530

    feat: Implement custom QuickSort in Intersection.java
    Replaced usage of Arrays.sort with a custom implementation of QuickSort
    in the Intersection.java file for sorting arrays.

--- dsa/src/main/java/com/dsa/algorithms/searching/Intersection.java
+++ dsa/src/main/java/com/dsa/algorithms/searching/Intersection.java
@@ -33,7 +33,7 @@ public class Intersection {
      * @return An array containing the intersection of the two input arrays.
      */
     static int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums2);
+        quickSort(nums2);
         Set<Integer> result = new HashSet<>();
 
         for (int n : nums1) {
@@ -48,6 +48,57 @@ public class Intersection {
             out[i++] = n;
         }
         return out;
+    }
+
+    static void quickSort(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+
+    /**
+     * Recursively sorts a portion of an array using the QuickSort algorithm.
+     * This is a helper method for the intersection finding logic.
+     *
+     * @param nums  The array of integers to be sorted.
+     * @param start The starting index of the portion to be sorted.
+     * @param end   The ending index of the portion to be sorted.
+     */
+    static void quickSort(int[] nums, int start, int end) {
+        if (start >= end)
+            return;
+
+        int pivotIndex = partition(nums, start, end);
+
+        quickSort(nums, start, pivotIndex - 1);
+        quickSort(nums, pivotIndex + 1, end);
+    }
+
+    /**
+     * Partitions a portion of an array around a pivot element.
+     * This is a helper method for the QuickSort algorithm used in intersection finding.
+     *
+     * @param nums  The array of integers to be partitioned.
+     * @param start The starting index of the portion to be partitioned.
+     * @param end   The ending index of the portion to be partitioned (the pivot element).
+     * @return The final index of the pivot element after partitioning.
+     */
+    static int partition(int[] nums, int start, int end) {
+        int pivot = nums[end];
+        int i = start - 1;
+
+        for (int j = start; j < end; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        int temp = nums[i + 1];
+        nums[i + 1] = nums[end];
+        nums[end] = temp;
+
+        return i + 1;
     }
 
     /**
